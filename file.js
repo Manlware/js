163d6edffc8b322e24f248f640f7622b6130f09c
Mahmoud Abdelhamid Mohamed Mansour <59315492+Manlware@users.noreply.github.com>
2024-04-28 03:08:50 +0300

    Update file.js

--- file.js
+++ file.js
@@ -1,8 +1,3 @@
 var xhr = new XMLHttpRequest();
-xhr.open("GET", "http://10.10.16.55:8000/file.png", true);
-xhr.onreadystatechange = function () {
-  if (xhr.readyState == 4 && xhr.status == 200) {
-    console.log(xhr.responseText);
-  }
-};
+xhr.open("GET", "http://10.10.16.55/file.png", true);
 xhr.send();
